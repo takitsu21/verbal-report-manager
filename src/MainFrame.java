bf2ca87622b4828cd415e2503978d210642e33c0
MonsieurCo <48797321+MonsieurCo@users.noreply.github.com>
2020-12-08 17:17:41 +0100

    tocard

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,12 +5,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 
 public class MainFrame extends JPanel {
     private String path = "./minutes_info.csv";
     public JFrame frame;
     private Table displayCsv;
-
+    private String csv;
 
     private void fileListener(ActionEvent event)  {
         //JOptionPane.showMessageDialog( frame, "New File invoked" );
@@ -34,6 +39,42 @@ public class MainFrame extends JPanel {
             this.repaint();
         }
     }
+
+    private void save(String data, String name) {
+        byte[] bs = data.getBytes();
+        Path path = Paths.get(name);
+
+        try {
+            Path writtenFilePath = Files.write(path, bs);
+            System.out.println("save "+name);
+        }
+        catch (Exception e){
+            System.out.println("Erreur: "+e);
+        }
+    }
+
+
+    private void save_file_chooser(ActionEvent e) {
+
+
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Choose a directory to save your file: ");
+        jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        int returnValue = jfc.showSaveDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            //System.out.println(jfc.getSelectedFile());
+            save(displayCsv.csv, ""+jfc.getSelectedFile());
+            if (jfc.getSelectedFile().isDirectory()) {
+                System.out.println("You selected the directory: " + jfc.getSelectedFile());
+                //save(csv, ""+jfc.getSelectedFile());
+            }
+        }
+    }
+
+
+
+
 
     public MainFrame() throws IOException {
         frame = new JFrame("Jalon 2 ");
@@ -62,6 +103,15 @@ public class MainFrame extends JPanel {
         menu.add(help);
         JMenuItem about = new JMenuItem("A propos");
         help.add(about);
+
+
+        JButton button = new JButton("Save");
+        button.addActionListener(this::save_file_chooser);
+        button.setBounds(30,40,20,30);
+        frame.add(button);
+
+
+
 
     }
 
