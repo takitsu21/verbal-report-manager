628eefe593392f5a28c984fbe5f6e2394f65685b
MonsieurCo <48797321+MonsieurCo@users.noreply.github.com>
2020-12-26 17:07:00 +0100

    better version of the bouton add course

--- src/com/mad/listener/AddCourseListener.java
+++ src/com/mad/listener/AddCourseListener.java
@@ -1,48 +1,68 @@
 package com.mad.listener;
 
-import com.mad.AbstractPopUpCours;
+import com.mad.AbstractApplication;
+import com.mad.util.Table;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 
-public class AddCourseListener extends AbstractPopUpCours implements ActionListener {
+public class AddCourseListener extends AbstractApplication implements ActionListener {
+    private JFrame tmp;
+    private JTextField idField;
+    private JTextField courseNameFiled;
+    private JTextField coefField;
+
+    public JTextField getIdField() {
+        return idField;
+    }
+
+    public JTextField getCourseNameFiled() {
+        return courseNameFiled;
+    }
+
+    public JTextField getCoefField() {
+        return coefField;
+    }
+
     @Override
+
     public void actionPerformed(ActionEvent e) {
-        setTmp(new JFrame("Ajouter un Cours"));
-        getTmp().setSize(300, 200);
-        getTmp().setLocationRelativeTo(null);
-        getTmp().setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setIdLabel(new JLabel("Identifiant:"));
-        setIdField(new JTextField(10));
-        setCourseLabel(new JLabel("Nom:         "));
-        setCourseNameFiled(new JTextField(10));
-        setCoefLabel(new JLabel("coefficient:"));
-        setCoefField(new JTextField(10));
+        tmp = new JFrame("Ajouter un Cours");
+        tmp.setSize(300, 200);
+        tmp.setLocationRelativeTo(null);
+        tmp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JLabel idLabel = new JLabel("Identifiant:");
+        idField = new JTextField(10);
+        JLabel courseLabel = new JLabel("Nom:         ");
+        courseNameFiled = new JTextField(10);
+        JLabel coefLabel = new JLabel("coefficient:");
+        coefField = new JTextField(10);
 
 
-        Container contentPane =  getTmp().getContentPane();
-        setGrille( new JPanel(new GridLayout(3, 1, 5, 1)));
+        Container contentPane = tmp.getContentPane();
+        JPanel Grille = new JPanel(new GridLayout(3, 1, 5, 1));
 
-        setP1(new JPanel());
-        getP1().add(getIdLabel());
-        getP1().add(getIdField());
-        getGrille().add(getP1());
+        JPanel P1 = new JPanel();
+        P1.add(idLabel);
+        P1.add(idField);
+        Grille.add(P1);
 
-        setP2(new JPanel());
-        getP2().add(getCourseLabel());
-        getP2().add(getCourseNameFiled());
-        getGrille().add(getP2());
+        JPanel P2 = new JPanel();
+        P2.add(courseLabel);
+        P2.add(courseNameFiled);
+        Grille.add(P2);
 
-       setP3( new JPanel());
-        getP3().add(getCoefLabel());
-        getP3().add(getCoefField());
-        getGrille().add(getP3());
+        JPanel P3 = new JPanel();
+        P3.add(coefLabel);
+        P3.add(coefField);
+        Grille.add(P3);
 
         contentPane.setLayout(new BorderLayout());
-        contentPane.add(getGrille(), BorderLayout.CENTER);
+        contentPane.add(Grille, BorderLayout.CENTER);
         JPanel southPane = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JButton validate = new JButton("valider");
         validate.addActionListener(new ValidateCourseListener());
@@ -50,10 +70,33 @@ public class AddCourseListener extends AbstractPopUpCours implements ActionListe
         contentPane.add(southPane, BorderLayout.SOUTH);
 
 
-        getTmp().setVisible(true);
+        tmp.setVisible(true);
 
 
     }
 
 
+    public class ValidateCourseListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String id = getIdField().getText();
+            String name =  getCourseNameFiled().getText();
+            String coef = getCoefField().getText();
+            System.out.println(id);
+            System.out.println(name);
+            System.out.println(coef);
+            if (getXmlEditor().addCourseGeneral(name, id, coef)) {
+                System.out.println("tnmù");
+                getXmlEditor().save("./xml-editor.tmp.xml");
+                Table.table.getModel().removeTableModelListener(new TableChangedListener());
+                OpenFileListener.openFile(new File("./xml-editor.tmp.xml"));
+                tmp.dispose();
+            }
+
+
+        }
+    }
+
+
 }
+
