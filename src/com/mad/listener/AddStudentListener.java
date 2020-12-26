914c831d5952c8e8ef02054b00fe1529638876f0
Margaux Schmied <margaux.schmied@icloud.com>
2020-12-26 14:11:22 +0100

    probleme de layout

--- src/com/mad/listener/AddStudentListener.java
+++ src/com/mad/listener/AddStudentListener.java
@@ -5,18 +5,22 @@ import com.mad.util.XmlToCsv;
 import org.w3c.dom.Element;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddStudentListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        JFrame tmp = new JFrame("Vue hiérarchisé");
-        tmp.setSize(600, 300);
-        tmp.setLocationRelativeTo(null);
-        tmp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JFrame frame = new JFrame("Ajouter un étudiant");
+        frame.setSize(800, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLayout(new GridLayout(2, 2));
 
         JPanel pnl = new JPanel();
         //pnl.setLayout(new BorderLayout());
@@ -41,32 +45,166 @@ public class AddStudentListener implements ActionListener {
         pnl.add(new JLabel("Program:"));
         pnl.add(program);
 
-        tmp.add(pnl, BorderLayout.NORTH);
+        frame.add(pnl, BorderLayout.NORTH);
 
         List<Element> listCourses = Data.getChildren(Data.root, "course");
 
-        JCheckBox[] cours= new JCheckBox[listCourses.size()+1];
-        JPanel panel = new JPanel();
-        JScrollPane scroll = new JScrollPane();
-        scroll.setPreferredSize(new Dimension(450, 110));
-        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        String[] cours= generateCheckboxValues(listCourses);
+
+        /*container.add(new CheckBoxGroup("COURS", cours), BorderLayout.SOUTH);
+        container.add(new CheckBoxGroup("OPTIONS", cours), BorderLayout.SOUTH);
+        container.add(new CheckBoxGroup("COMPOSANTES", cours), BorderLayout.SOUTH);*/
+
+        frame.getContentPane().add(new CheckBoxGroup("COURS", cours), BorderLayout.CENTER);
+        JLabel l = new JLabel("Ce cours peut être ajouté mais ne fait pas parti du programme courant");
+        l.setForeground(Color.decode("0xf27e11"));
+        frame.getContentPane().add(l,BorderLayout.SOUTH);
+
+        Container container = new Container();
+        container.add(new JLabel("test"));
+//        name.add(new JTextField());
+        frame.getContentPane().add(container);
 
 
-        cours[0]=new JCheckBox("Select All");
-        scroll.add(cours[0]);
 
-        for (int i=0; i<listCourses.size(); i++){
-            cours[i]=new JCheckBox(XmlToCsv.read(listCourses.get(i), "identifier") + " - " + XmlToCsv.read(listCourses.get(i), "name"));
 
-            panel.add(cours[i]);
-            System.out.println(cours[i].getText());
+    }
+
+    private String[] generateCheckboxValues(List<Element> courses) {
+        String[] values = new String[courses.size()];
+        int acc = 0;
+        for (Element course : courses) {
+            String courseName = XmlToCsv.read(course, "name");
+            String courseId = XmlToCsv.read(course, "identifier");
+            values[acc++] = String.format("%s - %s", courseId, courseName);
+        }
+        return values;
+    }
+
+
+    public class CheckBoxGroup extends JPanel {
+
+        private JCheckBox all;
+        private List<JCheckBox> checkBoxes;
+
+        public CheckBoxGroup(String labelChoice, String... options) {
+            checkBoxes = new ArrayList<>(25);
+            setLayout(new BorderLayout());
+            JPanel header = new JPanel(new FlowLayout(FlowLayout.LEFT, 1, 1));
+            all = new JCheckBox("Tout sélectionner...");
+            all.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    for (JCheckBox cb : checkBoxes) {
+                        cb.setSelected(all.isSelected());
+                    }
+                }
+            });
+            header.add(new Label(labelChoice));
+            header.add(all);
+            add(header, BorderLayout.NORTH);
+
+
+            JPanel content = new ScrollablePane(new GridBagLayout());
+            content.setBackground(UIManager.getColor("List.background"));
+            if (options.length > 0) {
+
+                GridBagConstraints gbc = new GridBagConstraints();
+                gbc.gridwidth = GridBagConstraints.REMAINDER;
+                gbc.anchor = GridBagConstraints.NORTHWEST;
+                gbc.weightx = 1;
+                for (int index = 0; index < options.length ; index++) {
+
+                    JCheckBox cb = new JCheckBox(options[index]);
+                    cb.setOpaque(false);
+                    //cb.setBackground(Color.GRAY);
+                    cb.setForeground(Color.decode("0xf27e11"));
+                    for (int i=0; i < Data.dataArray[0].length; i++){
+                        if(options[index].equals(Data.dataArray[0][i])){
+                            System.out.println(options[index]);
+                            //cb.setBackground(Color.WHITE);
+                            cb.setForeground(Color.BLACK);
+
+                        }
+                    }
+
+                    checkBoxes.add(cb);
+                    content.add(cb, gbc);
+                }
+
+                //JCheckBox cb = new JCheckBox(options[options.length - 1]);
+                //cb.setOpaque(false);
+                //checkBoxes.add(cb);
+                //gbc.weighty = 1;
+                //content.add(cb, gbc);
+
+            }
+
+            add(new JScrollPane(content));
+
+
         }
 
-        scroll.add(panel);
+        public class ScrollablePane extends JPanel implements Scrollable {
 
-        tmp.add(scroll, BorderLayout.CENTER);
+            public ScrollablePane(LayoutManager layout) {
+                super(layout);
+            }
 
-        tmp.setVisible(true);
+            public ScrollablePane() {
+            }
+
+            @Override
+            public Dimension getPreferredScrollableViewportSize() {
+                return new Dimension(100, 100);
+            }
+
+            @Override
+            public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+                return 32;
+            }
+
+            @Override
+            public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+                return 32;
+            }
+
+            @Override
+            public boolean getScrollableTracksViewportWidth() {
+                boolean track = false;
+                Container parent = getParent();
+                if (parent instanceof JViewport) {
+                    JViewport vp = (JViewport) parent;
+                    track = vp.getWidth() > getPreferredSize().width;
+                }
+                return track;
+            }
+
+            @Override
+            public boolean getScrollableTracksViewportHeight() {
+                boolean track = false;
+                Container parent = getParent();
+                if (parent instanceof JViewport) {
+                    JViewport vp = (JViewport) parent;
+                    track = vp.getHeight() > getPreferredSize().height;
+                }
+                return track;
+            }
+
+        }
+
+    }
+}
+
+
+class CheckboxAction extends AbstractAction {
+    public CheckboxAction(String text) {
+        super(text);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JCheckBox cbLog = (JCheckBox) e.getSource();
+        System.out.println(cbLog.getText());
     }
 }
